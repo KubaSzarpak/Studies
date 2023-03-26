9e6b0da5ff96043bf31f46709925d666b56986dc
Jakub Szarpak <kuba.szarpak02@gmail.com>
2023-03-26 01:10:45 +0100

    UTP directory fixed #2

--- Semestr III/UTP/Wyklady/Strumienie/samples/Encoding/DefaultEncoding.java
+++
@@ -1,9 +0,0 @@
- 
-public class DefaultEncoding {
- 
-  public static void main(String[] args) {
-    String p = System.getProperty("file.encoding");
-    System.out.println("Domyœlna strona kodow¹ jest œwieæ " + p);
-  } 
- 
-}
